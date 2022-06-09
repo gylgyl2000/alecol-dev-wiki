cc6bde20aa6c2b93f1af63df28573803363dcc6d
gylgyl2000 <gylgyl2000@gmail.com>
2022-06-09 09:15:34 +0200

    init

---
+++ docusaurus.config.js
@@ -0,0 +1,156 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Alécol',
+  tagline: '(Apprentissage Lecture & Calcul On-Line)',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/cropped-logo-Alecol.png',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Frederic7101', // Usually your GitHub org/user name.
+  projectName: 'RepoAlecol', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'fr',
+    locales: ['fr'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Alécol',
+        logo: {
+          alt: 'alecol',
+          src: 'img/cropped-logo-Alecol.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'Installation/installation',
+            position: 'left',
+            label: 'Installation',
+          },
+          {
+            type: 'doc',
+            docId: 'Backend_Serveur/Serveur',
+            position: 'left',
+            label: 'Backend',
+          },
+          {
+            type: 'doc',
+            docId: 'Frontend_Application/Application/index_html',
+            position: 'left',
+            label: 'Frontend',
+          },
+          {
+            type: 'doc',
+            docId: 'API/api',
+            position: 'left',
+            label: 'API',
+          },
+          {
+            type: 'doc',
+            docId: 'Documents/Conception_detaillee',
+            position: 'left',
+            label: 'Documents',
+          },
+          /* {to: '/blog', label: 'Blog', position: 'left'}, */
+          {
+            href: 'https://github.com/Frederic7101/RepoAlecol',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Aide',
+            items: [
+              {
+                label: 'Docusaurus',
+                href: 'https://docusaurus.io/fr/docs',
+              },
+              /* {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              }, */
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              /* {
+                label: 'Blog',
+                to: '/blog',
+              }, */
+              {
+                label: 'GitHub',
+                href: 'https://github.com/Frederic7101/RepoAlecol',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Alécol, Inc. Créé avec Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ src/pages/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        {/* <h1 className="hero__title">{siteConfig.title}</h1> */}
+        <img src="img/logo1.svg" height="120px"></img>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/menu">
+            Menu
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Bienvenue sur ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
